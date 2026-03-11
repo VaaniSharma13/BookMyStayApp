3afd71a137097d220037854df9a728ab6d0d9872
VaaniSharma13 <vaanisharma1309@gmail.com>
2026-03-11 15:32:18 +0530

    vaani added UC1

---
+++ BookMyStay/src/BookMyStayApp.java
@@ -0,0 +1,26 @@
+public class BookMyStayApp {
+    /**
+     * =====================================================
+     * MAIN CLASS - BookMyStayApp
+     * =====================================================
+     *
+     * Use Case 1: Application Entry & Welcome Message
+     *
+     * @author vaani
+     * @version 1.0
+     */
+
+    public class BookMyStay {
+
+        /**
+         * Application entry point
+         * @vaani args Command-line arguments
+         */
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to the Hotel Booking Management System");
+            System.out.println("System initialized successfully.");
+
+        }
+    }
+}
